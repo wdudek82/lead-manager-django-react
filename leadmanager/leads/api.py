e348e569a7bb44f38c0498606b7c58752ce592ee
Wojciech Dudek <wdudek82@gmail.com>
2019-02-01 23:40:39 +0100

    Init

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets, permissions
+
+from .models import Lead
+from .serializers import LeadSerializer
+
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permissions_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
