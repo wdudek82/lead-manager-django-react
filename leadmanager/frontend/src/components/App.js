a8b229372924ddaf38e2d2d4db25fc375d110ff3
Wojciech Dudek <wdudek82@gmail.com>
2019-02-03 01:03:30 +0100

    Added react frontend

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <h1>React App</h1>
+    return (
+      <div>
+        <h1>React App</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, voluptatem.</p>
+      </div>
+
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+export default App;
